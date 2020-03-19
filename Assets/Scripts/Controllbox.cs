36b9309d5962d2fe2d487675f6e93f22cf6c2d85
BeauTaapken <beautaapken@me.com>
2020-03-19 16:47:30 +0100

    * Added a weapon with animation to make it look like the weapon slices a mesh instead of a plane * Made it so slicing is only available when the weapon is in the neutral state

--- Assets/Scripts/Controllbox.cs
+++ Assets/Scripts/Controllbox.cs
@@ -4,23 +4,36 @@ using UnityEngine;
 
 public class Controllbox : MonoBehaviour
 {
+    public GameObject weapon;
     public GameObject withCuttingScript;
+    public string animationName;
 
     private Cutting cutting;
+    private Animator animator;
+    private bool isSlashing;
 
     // Start is called before the first frame update
     void Start()
     {
         cutting = withCuttingScript.GetComponent<Cutting>();
+        animator = weapon.GetComponent<Animator>();
+        animator.SetBool("isSlashing", false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Mathf.Approximately(Input.GetAxis("Fire1"), 1))
+        if (Mathf.Approximately(Input.GetAxis("Fire1"), 1) && !isSlashing && !animator.GetCurrentAnimatorStateInfo(0).IsName(animationName))
         {
+            isSlashing = true;
+            animator.SetBool("isSlashing", true);
             cutting.Slice();
         }
-        
+
+        if (Mathf.Approximately(Input.GetAxis("Fire1"), 0) && isSlashing)
+        {
+            isSlashing = false;
+            animator.SetBool("isSlashing", false);
+        }
     }
 }
