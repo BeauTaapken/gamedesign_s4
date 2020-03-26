f72a3260275e5e7e6a8c680a53691820492134ad
BeauTaapken <beautaapken@me.com>
2020-03-26 15:03:42 +0100

    * Started working on a healthbar * Added amount of enemies still alive out of

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Reflection.Emit;
+using TMPro;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
@@ -10,7 +11,8 @@ public class SpawnManager : MonoBehaviour
     public List<GameObject> SpawnableBosses;
     public int MonsterAmount = 3;
     public int BossAmount = 0;
-    public Camera PlayerCamera;
+    public TextMeshProUGUI tmpMonstersSlain;
+    public TextMeshProUGUI tmpBossesSlain;
 
     private float _planeX;
     private float _planeZ;
@@ -22,7 +24,8 @@ public class SpawnManager : MonoBehaviour
         _planeX = planeMesh.bounds.size.x;
         _planeZ = planeMesh.bounds.size.z;
         spawn(SpawnableMonsters);
-        spawn(SpawnableBosses);
+        //spawn(SpawnableBosses);
+        tmpMonstersSlain.text = MonsterAmount + "/" + MonsterAmount;
     }
 
     private void spawn(List<GameObject> list)
@@ -30,7 +33,7 @@ public class SpawnManager : MonoBehaviour
         for (int monsterIndex = 0; monsterIndex < MonsterAmount; monsterIndex++)
         {
             int monsterNumber = Random.Range(0, list.Count);
-
+            
             float _randomX = Random.Range(-_planeX / 2.0f, _planeX / 2.0f);
             float _randomZ = Random.Range(-_planeZ / 2.0f, _planeZ / 2.0f);
 
