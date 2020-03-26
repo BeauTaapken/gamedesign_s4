5c85690362b5a03ef871a3a53a800e074a1a28b1
brian <brian.vullers@home.nl>
2020-03-26 10:47:03 +0100

    Add basic movment x+z posision
    using horizontal and vertical axis

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public CharacterController controller;
 
-    // Update is called once per frame
+    public float speed = 12f;
     void Update()
     {
-        
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
     }
 }
