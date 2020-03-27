b89cc9f8d5a2bc3b5e9e22481b4da03ea24673d6
brian <brian.vullers@home.nl>
2020-03-27 14:50:35 +0100

    add basic pathfinding
    Enemy wil go to Player

---
+++ Assets/Scripts/EnamyController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnamyController : MonoBehaviour
+{
+    public GameObject Player;
+    public NavMeshAgent agent;
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(Player.transform.position);
+    }
+}
