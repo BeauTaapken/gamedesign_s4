1c3ed82f6fde026a64dcc18ab56d19d8d7917f3f
BeauTaapken <beautaapken@me.com>
2020-04-16 12:00:46 +0200

    * Monsters and bosses now have 3 different colors they can spawn with

---
+++ Assets/Scripts/RandomSkinColor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSkinColor : MonoBehaviour
+{
+    public Material[] skinColorMaterials;
+
+    private SkinnedMeshRenderer skinnedMeshRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int skinColorRange = Random.Range(0, skinColorMaterials.Length);
+        skinnedMeshRenderer = GetComponent<SkinnedMeshRenderer>();
+        Material[] mats = { skinColorMaterials[skinColorRange] };
+        skinnedMeshRenderer.materials = mats;
+    }
+}
